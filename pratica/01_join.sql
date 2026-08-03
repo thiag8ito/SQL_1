33ce1335c129a168e26675180cd8792f57acd8ba
thiag8ito <thiagoryoitoprog@gmail.com>
2026-08-03 16:59:32 -0300

    commit fora de casa

---
+++ pratica/01_join.sql
@@ -0,0 +1,13 @@
+-- Qual categoria tem mais produtos vendidos?
+
+SELECT 
+       t2.DescCategoriaProduto,
+       count(distinct t1.IdTransacao)
+
+FROM transacao_produto AS t1
+
+LEFT JOIN produtos AS t2
+ON t1.IdProduto = t2.IdProduto
+
+GROUP BY t2.DescCategoriaProduto
+ORDER BY count(distinct t1.IdTransacao) DESC
